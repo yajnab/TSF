e666fd4ae59400ed6252b25f9b6c585f350836d3
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2017-04-13 18:56:55 +0530

    arima.py: AutoCorrelation Plot

--- arima.py
+++ arima.py
@@ -1,6 +1,7 @@
 from pandas import read_csv
 from pandas import datetime
 from matplotlib import pyplot
+from pandas.tools.plotting import autocorrelation_plot
 
 def parser(x):
 	return datetime.strptime('190'+x, '%Y-%m')
@@ -8,4 +9,6 @@ def parser(x):
 series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
 print(series.head())
 series.plot()
+autocorrelation_plot(series)
+pyplot.show()
 pyplot.show()
